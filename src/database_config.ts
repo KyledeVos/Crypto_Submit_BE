cd3769a2e1cd380631cdfe6362b48335987a5dfb
Kyle <kdevos827@gmail.com>
2025-08-23 18:54:40 +0200

    Database config to retrieve and validate using .env values with Unit Tests Passed

--- src/database_config.ts
+++ src/database_config.ts
@@ -15,9 +15,48 @@ export const validateSetDatabaseConnectValues = (
     dbUser: string | undefined,
     dbPassword: string | undefined,
     dbConnectionLimit: string | undefined 
-): string => {
+): {error: boolean, message?: string} => {
 
-    return "success"
+    try{
+        // blank or missing checks
+        if(!dbHostName || dbHostName === undefined || typeof dbHostName !== "string" || dbHostName.trim() === ""){
+            return {error: true, message: "Missing DB Host Name"}
+        }else if(!dbUser || dbUser === undefined || typeof dbUser !== "string" || dbUser.trim() === ""){
+            return {error: true, message: "Missing DB User"}
+        }else if(!dbPassword || dbPassword === undefined || typeof dbPassword !== "string" || dbPassword.trim() === ""){
+            return {error: true, message: "Missing DB Password"}
+        }else if(!dbConnectionLimit || dbConnectionLimit === undefined || typeof dbConnectionLimit !== "string" || dbConnectionLimit.trim() === ""){
+            return {error: true, message: "Missing DB Connection Limit"}
+        }
+
+        let dbConnectionLimitNumber: number | undefined = undefined
+
+        // attempt to cast connection limit to an integer
+        try{
+            dbConnectionLimitNumber = Number(dbConnectionLimit)
+            if(!Number.isInteger(dbConnectionLimitNumber) || Number.isNaN(dbConnectionLimitNumber)){
+                return {error: true, message: "Database Connection Limit is not a Valid Integer"}
+            }else if(dbConnectionLimitNumber <= 0){
+                return {error: true, message: "Database Connection Limit is less than or equal to 0"}
+            }
+        }catch(error){
+            return {error: true, message: `Error occured in validateSetDatabaseConnectValues for connection limit cast as: ${error}`}
+        }
+
+        // Data is Valid - set data and return no error
+        dbHostNameValidated = dbHostName
+        dbUserValidated = dbUser
+        dbPasswordValidated = dbPassword
+        dbConnectionLimitValidated = dbConnectionLimitNumber
+
+        return {error: false}
+
+    }catch(error){
+        return {error: true, message: `Error occured in validateSetDatabaseConnectValues as: ${error}`}
+    }
+
+
+
 
 }
 
--- tests/setup_tests/database_middleware.test.ts
+++ tests/setup_tests/database_middleware.test.ts
@@ -8,7 +8,63 @@ import {describe, test, expect} from "@jest/globals"
 
 describe('testing database connection middleware', ()=>{
     test('Success Test', () => {
+        const expectedResult = {error: false}
+        expect(validateSetDatabaseConnectValues('localhost', 'root', '12345', '10')).toEqual(expectedResult)
+    })
 
-        expect(validateSetDatabaseConnectValues('localhost', 'root', '12345', '10')).toEqual("success")
+    test('Fail Case 1 - missing host', () => {
+        const expectedResult = {error: true, message: "Missing DB Host Name"}
+        expect(validateSetDatabaseConnectValues(undefined, 'root', '12345', '10')).toEqual(expectedResult)
+    })
 
-    })})
+    test('Fail Case 2 - missing user', () => {
+        const expectedResult = {error: true, message: "Missing DB User"}
+        expect(validateSetDatabaseConnectValues("localhost", undefined, '12345', '10')).toEqual(expectedResult)
+    })
+
+    test('Fail Case 3 - missing password', () => {
+        const expectedResult = {error: true, message: "Missing DB Password"}
+        expect(validateSetDatabaseConnectValues("localhost", 'root', undefined, '10')).toEqual(expectedResult)
+    })
+
+    test('Fail Case 4 - missing connection limit', () => {
+        const expectedResult = {error: true, message: "Missing DB Connection Limit"}
+        expect(validateSetDatabaseConnectValues("localhost", 'root', '12345', undefined)).toEqual(expectedResult)
+    })
+
+    test('Fail Case 5 - blank host name', () => {
+        const expectedResult = {error: true, message: "Missing DB Host Name"}
+        expect(validateSetDatabaseConnectValues("", 'root', '12345', '10')).toEqual(expectedResult)
+    })
+
+    test('Fail Case 6 - blank user', () => {
+        const expectedResult = {error: true, message: "Missing DB User"}
+        expect(validateSetDatabaseConnectValues("localhost", '', '12345', '10')).toEqual(expectedResult)
+    })
+
+    test('Fail Case 7 - blank password', () => {
+        const expectedResult = {error: true, message: "Missing DB Password"}
+        expect(validateSetDatabaseConnectValues("localhost", 'root', '', '10')).toEqual(expectedResult)
+    })
+
+    
+    test('Fail Case 8 - blank connection limit', () => {
+        const expectedResult = {error: true, message: "Missing DB Connection Limit"}
+        expect(validateSetDatabaseConnectValues("localhost", 'root', '12345', '')).toEqual(expectedResult)
+    })
+
+    test('Fail Case 9 - connection limit as non-number', () => {
+        const expectedResult = {error: true, message: "Database Connection Limit is not a Valid Integer"}
+        expect(validateSetDatabaseConnectValues("localhost", 'root', '12345', 'abc')).toEqual(expectedResult)
+    })
+
+    test('Fail Case 10 - connection limit as 0', () => {
+        const expectedResult = {error: true, message: "Database Connection Limit is less than or equal to 0"}
+        expect(validateSetDatabaseConnectValues("localhost", 'root', '12345', '0')).toEqual(expectedResult)
+    })
+
+    test('Fail Case 11 - connection limit as less than 0', () => {
+        const expectedResult = {error: true, message: "Database Connection Limit is less than or equal to 0"}
+        expect(validateSetDatabaseConnectValues("localhost", 'root', '12345', '-10')).toEqual(expectedResult)
+    })
+})
